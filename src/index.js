4f15206455edf0693865baebd0dd076ff6faa805
Logan Bernard <bernard.logan4@gmail.com>
2017-04-18 16:09:31 -0400

    init for npm link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-export default ''
+import request from 'request-promise'
+
+const debug = require('debug')('messenger-api')
+class Api {
+  constructor(ACCESS_TOKEN) {
+    this.ACCESS_TOKEN = ACCESS_TOKEN
+    this.BASE_URL = 'https://graph.facebook.com/v2.8/'
+  }
+
+  subscribe = async () => {
+    const res = await request({
+      url: `${this.BASE_URL}/me/subscribed_apps`,
+      qs: { access_token: this.ACCESS_TOKEN },
+      method: 'POST'
+    })
+    if (res.error) {
+      throw new Error(body.error)
+    }
+    else {
+      debug(`Successfully subscribed to page: ${res}`)
+    }
+  }
+
+  getUserProfile = async (id) => {
+    return await request({
+      url: `${this.BASE_URL}/${id}`,
+      qs: {
+        access_token: this.ACCESS_TOKEN,
+        fields: 'first_name,last_name,profile_pic,locale,timezone,gender,is_payment_enabled'
+      },
+      method: 'GET'
+    })
+  }
+}
+
+export default Api
